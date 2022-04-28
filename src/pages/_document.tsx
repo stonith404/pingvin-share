e0669979e2b98d3d0d698327d33846093664382b
Elias Schneider <login@eliasschneider.com>
2022-04-28 08:19:55 +0200

    added plausible

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,8 +1,26 @@
-import Document from "next/document";
 import { createGetInitialProps } from "@mantine/next";
+import Document, { Head, Html, Main, NextScript } from "next/document";
 
 const getInitialProps = createGetInitialProps();
 
 export default class _Document extends Document {
   static getInitialProps = getInitialProps;
+
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+        {/* Plausible analytics */}
+        <script
+          defer
+          data-domain="pingvin-share.dev.eliasschneider.com"
+          src="https://analytics.eliasschneider.com/js/plausible.js"
+        ></script>
+      </Html>
+    );
+  }
 }
