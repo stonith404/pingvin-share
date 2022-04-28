22bca7692ec1f4722645df15bd9a23d25cb79a95
Elias Schneider <login@eliasschneider.com>
2022-04-28 15:22:25 +0200

    Remove PWA build files

--- next.config.js
+++ next.config.js
@@ -5,9 +5,7 @@ const withPWA = require("next-pwa");
 const nextConfig = withPWA({
   reactStrictMode: true,
   pwa: {
-    dest: "public",
-    register: true,
-    skipWaiting: true,
+    dest: "public"
   },
 })
 
