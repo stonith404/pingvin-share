d4da3cae30dbce00832dc34f1c0d7d8c1135830f
Elias Schneider <login@eliasschneider.com>
2022-05-12 16:25:34 +0200

    Improve setup script

--- .setup/services/auth.service.ts
+++ .setup/services/auth.service.ts
@@ -3,6 +3,7 @@ import rl from "readline-sync";
 import cookie from "cookie";
 
 const getToken = async () => {
+  console.info("Please enter your Appwrite credentials \n");
   var email = rl.question("Email: ");
   var password = rl.question("Password: ", {
     hideEchoBack: true,
