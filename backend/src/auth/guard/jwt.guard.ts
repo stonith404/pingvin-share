d0901d497b0ddaf2826a6fa21c7ff8c3504ff09f
Elias Schneider <login@eliasschneider.com>
2022-10-29 22:48:00 +0200

    ix: jwt guard when unauthenticated shares are allowed

--- backend/src/auth/guard/jwt.guard.ts
+++ backend/src/auth/guard/jwt.guard.ts
@@ -1,16 +1,15 @@
 import { ExecutionContext } from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
-import { Observable } from "rxjs";
 
 export class JwtGuard extends AuthGuard("jwt") {
   constructor() {
     super();
   }
-  canActivate(
-    context: ExecutionContext
-  ): boolean | Promise<boolean> | Observable<boolean> {
-    return process.env.ALLOW_UNAUTHENTICATED_SHARES == "true"
-      ? true
-      : super.canActivate(context);
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    try {
+      return (await super.canActivate(context)) as boolean;
+    } catch {
+      return process.env.ALLOW_UNAUTHENTICATED_SHARES == "true";
+    }
   }
 }
