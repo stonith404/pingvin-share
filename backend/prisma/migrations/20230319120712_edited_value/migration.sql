ea052ce8d95b5b4b16dd6f13b2a836c6b520275d
Elias Schneider <login@eliasschneider.com>
2023-03-21 17:50:47 +0100

    refactor: merge two migrations into one

--- backend/prisma/migrations/20230319120712_edited_value/migration.sql
+++ backend/prisma/migrations/20230319120712_edited_value/migration.sql
@@ -5,8 +5,8 @@ CREATE TABLE "new_Config" (
     "name" TEXT NOT NULL,
     "category" TEXT NOT NULL,
     "type" TEXT NOT NULL,
-    "value" TEXT NOT NULL,
-    "editedValue" TEXT NOT NULL DEFAULT '',
+    "value" TEXT,
+    "defaultValue" TEXT NOT NULL DEFAULT '',
     "description" TEXT NOT NULL,
     "obscured" BOOLEAN NOT NULL DEFAULT false,
     "secret" BOOLEAN NOT NULL DEFAULT true,
@@ -18,8 +18,6 @@ CREATE TABLE "new_Config" (
 INSERT INTO "new_Config" ("category", "description", "locked", "name", "obscured", "order", "secret", "type", "updatedAt", "value") SELECT "category", "description", "locked", "name", "obscured", "order", "secret", "type", "updatedAt", "value" FROM "Config";
 DROP TABLE "Config";
 ALTER TABLE "new_Config" RENAME TO "Config";
-UPDATE "Config" SET "editedValue" = "value";
-UPDATE "Config" SET "value" = '';
 
 PRAGMA foreign_key_check;
 PRAGMA foreign_keys=ON;
