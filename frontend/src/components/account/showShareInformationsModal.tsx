b151f17e8b0fe995f5a3190752aef67de48afca7
pierrbt <pierre08bt@gmx.co.uk>
2023-06-23 23:17:40 +0200

    Adding description, disk usage

--- frontend/src/components/account/showShareInformationsModal.tsx
+++ frontend/src/components/account/showShareInformationsModal.tsx
@@ -1,4 +1,4 @@
-import {Text, Divider, Progress, Stack, Group} from '@mantine/core';
+import {Text, Divider, Progress, Stack, Group, Flex} from '@mantine/core';
 import { ModalsContextProps } from "@mantine/modals/lib/context";
 import { MyShare } from "../../types/share.type";
 import moment from "moment";
@@ -12,7 +12,7 @@ const showShareInformationsModal = (
   maxShareSize: number | string
 ) => {
   const link = `${appUrl}/share/${share.id}`;
-  const shareSize = 165564769; // TODO: get share size from backend
+  const shareSize = 565564769; // TODO: get share size from backend
   if (typeof maxShareSize === "string") {
     maxShareSize = parseInt(maxShareSize);
   }
@@ -21,46 +21,62 @@ const showShareInformationsModal = (
 
     children: (
       <Stack align="stretch">
-          <Text size="sm" color="lightgray" style={{ margin: '0.5rem 0' }}>
-            <b>ID:</b> {share.id}
-          </Text>
+        <Text size="sm" color="lightgray" style={{ margin: '0.5rem 0' }}>
+          <b>ID:</b> {share.id}
+        </Text>
 
-          <Text size="sm" color="lightgray" style={{ marginBottom: '0.5rem' }}>
-            <b>Creator :</b> You
-          </Text>
+        <Text size="sm" color="lightgray" style={{ marginBottom: '0.5rem' }}>
+          <b>Creator :</b> You
+        </Text>
 
-          <Text size="sm" color="lightgray" style={{ marginBottom: '0.5rem' }}>
-            <b>Created at :</b> {moment(share.createdAt).format("LLL")}
-          </Text>
+        <Text size="sm" color="lightgray" style={{ marginBottom: '0.5rem' }}>
+          <b>Description :</b> {share.description || "No description"}
+        </Text>
 
-          <Text size="sm" color="lightgray">
-            <b>Expires at :</b> {moment(share.expiration).format("LLL")}
-          </Text>
+        <Text size="sm" color="lightgray" style={{ marginBottom: '0.5rem' }}>
+          <b>Created at :</b> {moment(share.createdAt).format("LLL")}
+        </Text>
 
-          <Divider style={{ margin: '.5rem 0' }} />
+        <Text size="sm" color="lightgray">
+          <b>Expires at :</b> {moment(share.expiration).format("LLL")}
+        </Text>
 
-          <CopyTextField link={link} />
+        <Divider style={{ margin: '.5rem 0' }} />
 
-          <Divider style={{ margin: '1rem 0' }} />
+        <CopyTextField link={link} />
 
-          <Text size="sm" color="lightgray" style={{ marginBottom: '0.5rem' }}>
+        <Divider style={{ margin: '1rem 0' }} />
+
+        <Text size="sm" color="lightgray" style={{ marginBottom: '0.5rem' }}>
+          <span>
             <b>Size :</b> {byteToHumanSizeString(shareSize)} / {byteToHumanSizeString(maxShareSize)}
+          </span>
+
+          { /* Faire un flex pour aligner le Progress et le text à droite */}
+
+          <Flex justify="center" direction={{base: 'row'}} align="center" style={{alignItems: "center", marginTop: "1rem"}}>
+            { (shareSize / maxShareSize < 0.1) && (
+              <Text size="xs" color="lightgray" style={{ marginRight: '4px'}}>
+                {byteToHumanSizeString(shareSize)}
+              </Text>
+            ) }
             <Progress
               value={((shareSize / maxShareSize) * 100)}
-              label={byteToHumanSizeString(shareSize)}
+              label={(shareSize / maxShareSize >= 0.1) ? (byteToHumanSizeString(shareSize)) : ""}
               color="blue"
-              style={{ marginTop: '0.5rem' }}
+              style={{ width: (shareSize / maxShareSize < 0.1) ? '70%' : '80%' }}
               size="xl"
               radius="xl"
-            >
-              <Group
-                position="right">
-                <Text size="sm" color="lightgray" style={{ marginRight: '0.5rem' }}>
-                  {byteToHumanSizeString(maxShareSize)}
-                </Text>
-              </Group>
-            </Progress>
-          </Text>
+
+            />
+            <Text size="xs" color="lightgray" style={{ marginLeft: '4px'}}>
+              {byteToHumanSizeString(maxShareSize)}
+            </Text>
+          </Flex>
+
+
+
+        </Text>
 
       </Stack>
     ),
