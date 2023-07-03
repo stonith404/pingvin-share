3d1ba228789edc7f42aaa0a0abc9bf8b02dc527d
pierrbt <pierre08bt@gmx.co.uk>
2023-07-03 19:42:06 +0200

    fix: fixing migrations

--- backend/prisma/migrations/20230701124358_reverse_share_options/migration.sql
+++ backend/prisma/migrations/20230701124358_reverse_share_options/migration.sql
@@ -1,6 +1,5 @@
--- RedefineTables
-PRAGMA foreign_keys=OFF;
-CREATE TABLE "new_ReverseShareOptions" (
+-- CreateTable
+CREATE TABLE "ReverseShareOptions" (
     "id" TEXT NOT NULL PRIMARY KEY,
     "shareId" TEXT NOT NULL,
     "easyMode" BOOLEAN NOT NULL DEFAULT false,
@@ -10,9 +9,6 @@ CREATE TABLE "new_ReverseShareOptions" (
     "maximalViewsEnabled" BOOLEAN NOT NULL DEFAULT true,
     CONSTRAINT "ReverseShareOptions_shareId_fkey" FOREIGN KEY ("shareId") REFERENCES "ReverseShare" ("id") ON DELETE CASCADE ON UPDATE CASCADE
 );
-INSERT INTO "new_ReverseShareOptions" ("customLinkEnabled", "descriptionEnabled", "easyMode", "id", "maximalViewsEnabled", "passwordEnabled", "shareId") SELECT "customLinkEnabled", "descriptionEnabled", "easyMode", "id", "maximalViewsEnabled", "passwordEnabled", "shareId" FROM "ReverseShareOptions";
-DROP TABLE "ReverseShareOptions";
-ALTER TABLE "new_ReverseShareOptions" RENAME TO "ReverseShareOptions";
+
+-- CreateIndex
 CREATE UNIQUE INDEX "ReverseShareOptions_shareId_key" ON "ReverseShareOptions"("shareId");
-PRAGMA foreign_key_check;
-PRAGMA foreign_keys=ON;
