5816b39fc6ef6fe6b7cf8e7925aa297561f5b796
Elias Schneider <login@eliasschneider.com>
2023-07-22 15:29:53 +0200

    fix: confusion between GB and GiB

--- frontend/src/utils/fileSize.util.ts
+++ frontend/src/utils/fileSize.util.ts
@@ -1,17 +1,17 @@
 export function byteToHumanSizeString(bytes: number) {
   const sizes = ["B", "KB", "MB", "GB", "TB"];
   if (bytes == 0) return "0 Byte";
-  const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)).toString());
-  return (bytes / Math.pow(1024, i)).toFixed(1).toString() + " " + sizes[i];
+  const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1000)).toString());
+  return (bytes / Math.pow(1000, i)).toFixed(1).toString() + " " + sizes[i];
 }
 
 export function byteToUnitAndSize(bytes: number) {
   const units = ["B", "KB", "MB", "GB", "TB"];
   if (bytes == 0) return { unit: "B", size: 0 };
-  const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)).toString());
+  const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1000)).toString());
 
   return {
-    size: parseFloat((bytes / Math.pow(1024, i)).toFixed(1)),
+    size: parseFloat((bytes / Math.pow(1000, i)).toFixed(1)),
     unit: units[i],
   };
 }
@@ -19,5 +19,5 @@ export function byteToUnitAndSize(bytes: number) {
 export function unitAndSizeToByte(unit: string, size: number) {
   const units = ["B", "KB", "MB", "GB", "TB"];
   const i = units.indexOf(unit);
-  return Math.pow(1024, i) * size;
+  return Math.pow(1000, i) * size;
 }
