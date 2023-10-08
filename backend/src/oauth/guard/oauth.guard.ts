0c5498a103d2190d46c772841bcdd6b3e62a0a9d
Qing Fu <16662647+zz5840@users.noreply.github.com>
2023-10-08 15:07:49 +0800

    feat(oauth): oauth guard

--- backend/src/oauth/guard/oauth.guard.ts
+++ backend/src/oauth/guard/oauth.guard.ts
@@ -1,18 +1,25 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Reflector } from "@nestjs/core";
+import { ConfigService } from "../../config/config.service";
 
 @Injectable()
 export class OAuthGuard implements CanActivate {
-  constructor(private reflector: Reflector) {
+  constructor(
+    private reflector: Reflector,
+    private config: ConfigService,
+  ) {
   }
 
   canActivate(context: ExecutionContext): boolean {
-    const provider = this.reflector.get<string>("oauthProvider", context.getHandler())
-    const request = context.switchToHttp().getRequest();
-    if (request.query.state !== request.cookies[`oauth_${provider}_state`]) {
-      return false;
+    const oauth = this.reflector.get<{
+      provider: string;
+      type: "endpoint" | "callback";
+    }>("oauth", context.getHandler())
+    if (oauth.type === "callback") {
+      const request = context.switchToHttp().getRequest();
+      return request.query.state === request.cookies[`oauth_${oauth.provider}_state`];
+    } else {
+      return this.config.get(`oauth.${oauth.provider}-enabled`)
     }
-    // TODO validate nonce
-    return true;
   }
 }
