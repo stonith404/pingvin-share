c0064c9bad2d9451873f45bd8205fb8199e4b006
Elias Schneider <login@eliasschneider.com>
2023-11-04 20:46:56 +0100

    New translations en-us.ts (Danish)

--- frontend/src/i18n/translations/da-DK.ts
+++ frontend/src/i18n/translations/da-DK.ts
@@ -264,6 +264,12 @@ export default {
   "share.modal.file-preview.error.not-supported.title": "Forhåndsvisning ikke understøttet",
   "share.modal.file-preview.error.not-supported.description": "En forhåndsvisning for thise filtype er ikke understøttet. Download venligst filen for at se den.",
   // END /share/[id]
+  // /share/[id]/edit
+  "share.edit.title": "Edit {shareId}",
+  "share.edit.append-upload": "Append file",
+  "share.edit.notify.generic-error": "An error occurred while finishing your share.",
+  "share.edit.notify.save-success": "Share updated successfully",
+  // END /share/[id]/edit
   // /admin/config
   "admin.config.title": "Konfiguration",
   "admin.config.category.general": "Generelt",
