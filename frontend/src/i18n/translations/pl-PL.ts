b3b2979f60c9a3c9d28dc5b2a1463283e07d5c29
Elias Schneider <login@eliasschneider.com>
2023-11-04 20:46:59 +0100

    New translations en-us.ts (Polish)

--- frontend/src/i18n/translations/pl-PL.ts
+++ frontend/src/i18n/translations/pl-PL.ts
@@ -264,6 +264,12 @@ export default {
   "share.modal.file-preview.error.not-supported.title": "Podgląd nie jest obsługiwany",
   "share.modal.file-preview.error.not-supported.description": "Podgląd dla tego typu pliku nie jest obsługiwany. Pobierz plik, aby go zobaczyć.",
   // END /share/[id]
+  // /share/[id]/edit
+  "share.edit.title": "Edit {shareId}",
+  "share.edit.append-upload": "Append file",
+  "share.edit.notify.generic-error": "An error occurred while finishing your share.",
+  "share.edit.notify.save-success": "Share updated successfully",
+  // END /share/[id]/edit
   // /admin/config
   "admin.config.title": "Konfiguracja",
   "admin.config.category.general": "Ogólne",
