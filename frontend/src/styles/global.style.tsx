08f0dbbc4c0f8137a384224e59a0954766163373
Maurice Schorn <pxlmo.codes@gmail.com>
2024-01-30 19:22:14 +0100

    basic table styling

--- frontend/src/styles/global.style.tsx
+++ frontend/src/styles/global.style.tsx
@@ -8,6 +8,13 @@ const GlobalStyle = () => {
           color: "inherit",
           textDecoration: "none",
         },
+        "table.md, table.md th:nth-of-type(odd), table.md td:nth-of-type(odd)": {
+          background: "rgba(50, 50, 50, 0.5)",
+        },
+        "table.md td": {
+          paddingLeft: "0.5em",
+          paddingRight: "0.5em",
+        },
       })}
     />
   );
