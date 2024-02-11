e663da45b1d15f5e6e33118e6a28e1504688034c
Elias Schneider <login@eliasschneider.com>
2024-02-11 16:19:19 +0100

    fix: user `id` and `totpVerified` can't be changed by user

--- backend/src/user/dto/updateOwnUser.dto.ts
+++ backend/src/user/dto/updateOwnUser.dto.ts
@@ -1,6 +1,6 @@
-import { OmitType, PartialType } from "@nestjs/swagger";
+import { PartialType, PickType } from "@nestjs/swagger";
 import { UserDTO } from "./user.dto";
 
 export class UpdateOwnUserDTO extends PartialType(
-  OmitType(UserDTO, ["isAdmin", "password"] as const),
+  PickType(UserDTO, ["username", "email"] as const)
 ) {}
