5ad768e484878d9f21b82a91e029aec6373dc425
Elias Schneider <login@eliasschneider.com>
2024-05-04 00:59:37 +0300

    New translations en-us.ts (German)

--- frontend/src/i18n/translations/de-DE.ts
+++ frontend/src/i18n/translations/de-DE.ts
@@ -169,6 +169,7 @@ export default {
   // /admin
   "admin.title": "Verwaltung",
   "admin.button.users": "Benutzerverwaltung",
+  "admin.button.shares": "Share management",
   "admin.button.config": "Konfiguration",
   "admin.version": "Version",
   // END /admin
@@ -195,6 +196,15 @@ export default {
   "admin.users.modal.create.admin": "Administratorrechte",
   "admin.users.modal.create.admin.description": "Wenn aktiviert, kann der Benutzer auf das Administrator-Panel zugreifen.",
   // END /admin/users
+  // /admin/shares
+  "admin.shares.title": "Share management",
+  "admin.shares.table.id": "Share ID",
+  "admin.shares.table.username": "Creator",
+  "admin.shares.table.visitors": "Visitors",
+  "admin.shares.table.expires": "Expires At",
+  "admin.shares.edit.delete.title": "Delete share {id}",
+  "admin.shares.edit.delete.description": "Do you really want to delete this share?",
+  // END /admin/shares
   // /upload
   "upload.title": "Upload",
   "upload.notify.generic-error": "Während der Erstellung der Freigabe ist ein Fehler aufgetreten.",
