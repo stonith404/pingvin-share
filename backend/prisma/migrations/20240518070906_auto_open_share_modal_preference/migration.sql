80d1b41e3b9ed90ed1e810bde97d12464a72dd5a
Elias Schneider <login@eliasschneider.com>
2024-06-10 11:17:32 +0200

    fix: remove migration and add new config variable to seed script

--- backend/prisma/migrations/20240518070906_auto_open_share_modal_preference/migration.sql
+++
@@ -1,3 +0,0 @@
--- This is an empty migration.
-INSERT INTO Config ("category", "name" , "locked", "obscured", "order", "secret", "type", "defaultValue", "value", "updatedAt")
-VALUES ('share', 'autoOpenShareModal', false, false, 6, false, 'boolean', "true", NULL, CURRENT_TIMESTAMP);
