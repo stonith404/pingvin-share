2dac38560b6c54b6e7676dcd4682bfa57973292f
Elias Schneider <login@eliasschneider.com>
2024-06-30 20:10:16 +0200

    fix: set max age of access token cookie to 15 minutes

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -229,7 +229,7 @@ export class AuthService {
     if (accessToken)
       response.cookie("access_token", accessToken, {
         sameSite: "lax",
-        maxAge: 1000 * 60 * 60 * 24 * 30 * 3, // 3 months
+        maxAge: 1000 * 60 * 60 * 15, // 15 minutes
       });
     if (refreshToken)
       response.cookie("refresh_token", refreshToken, {
