6f4522210dd65a6e2d7e675ca9d117653495f7ba
thecrafterjt <mcsr@web.de>
2024-07-09 10:15:02 +0200

    Update uk-UA.ts

--- frontend/src/i18n/translations/uk-UA.ts
+++ frontend/src/i18n/translations/uk-UA.ts
@@ -344,6 +344,8 @@ export default {
   "admin.config.smtp.password": "Пароль",
   "admin.config.smtp.password.description": "Пароль SMTP-сервера",
   "admin.config.smtp.button.test": "Відправити тестовий лист",
+  "admin.config.smtp.allow-unauthenticated-certificates": "Trust unauthenticated SMTP server certificates",
+  "admin.config.smtp.allow-unauthenticated-certificates.description": "Only set this to true if you need to trust self signed certificates.",
   "admin.config.oauth.allow-registration": "Дозволити реєстрацію",
   "admin.config.oauth.allow-registration.description": "Дозволити користувачам реєструватися, використовуючи облікові записи соціальних мереж",
   "admin.config.oauth.ignore-totp": "Ігнорувати TOTP",
