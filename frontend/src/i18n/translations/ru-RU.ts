34388235b22cb1a9ad2698b4071b0284932bc6a1
Elias Schneider <login@eliasschneider.com>
2024-07-11 21:51:41 +0200

    New translations en-us.ts (Russian)

--- frontend/src/i18n/translations/ru-RU.ts
+++ frontend/src/i18n/translations/ru-RU.ts
@@ -345,6 +345,8 @@ export default {
   "admin.config.smtp.password": "Пароль",
   "admin.config.smtp.password.description": "Пароль SMTP-сервера",
   "admin.config.smtp.button.test": "Отправить тестовое письмо",
+  "admin.config.smtp.allow-unauthorized-certificates": "Trust unauthorized SMTP server certificates",
+  "admin.config.smtp.allow-unauthorized-certificates.description": "Only set this to true if you need to trust self signed certificates.",
   "admin.config.oauth.allow-registration": "Разрешить регистрацию",
   "admin.config.oauth.allow-registration.description": "Разрешить пользователям регистрироваться используя учетные записи социальных сетей",
   "admin.config.oauth.ignore-totp": "Игнорировать TOTP",
