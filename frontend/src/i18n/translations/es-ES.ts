db5242c6fd8d2fee87f342b78e385b8a6b71b6d1
Elias Schneider <login@eliasschneider.com>
2024-07-30 08:46:43 +0200

    New translations en-us.ts (Spanish)

--- frontend/src/i18n/translations/es-ES.ts
+++ frontend/src/i18n/translations/es-ES.ts
@@ -152,6 +152,10 @@ export default {
   "account.reverseShares.modal.max-size.label": "Tamaño máximo del compartido",
   "account.reverseShares.modal.send-email": "Enviar notificación por correo",
   "account.reverseShares.modal.send-email.description": "Enviar una notificación por correo cuando se comparta algo con este enlace de compartición inversa.",
+  "account.reverseShares.modal.simplified": "Simple mode",
+  "account.reverseShares.modal.simplified.description": "Make it easy for the person uploading the file to share it with you. They will be able to customize only the name and description of the share.",
+  "account.reverseShares.modal.public-access": "Public access",
+  "account.reverseShares.modal.public-access.description": "Make the created shares with this reverse share public. If disabled, only you and the creator of the share can view it.",
   "account.reverseShares.modal.max-use.label": "Máximo de usos",
   "account.reverseShares.modal.max-use.description": "Cantidad máxima de veces que esta URL se puede usar para crear un compartido.",
   "account.reverseShare.never-expires": "Esta compartición inversa nunca expirará.",
@@ -255,6 +259,7 @@ export default {
   "upload.modal.completed.never-expires": "Este compartido nunca expirará.",
   "upload.modal.completed.expires-on": "Este compartido expira en {expiration}.",
   "upload.modal.completed.share-ready": "Compartido listo",
+  "upload.modal.completed.notified-reverse-share-creator": "We have notified the creator of the reverse share. You can also manually share this link with them through other means.",
   // END /upload
   // /share/[id]
   "share.title": "Compartido {shareId}",
@@ -264,6 +269,8 @@ export default {
   "share.error.removed.title": "Compartido eliminado",
   "share.error.not-found.title": "Compartido no encontrado",
   "share.error.not-found.description": "El compartido que estás buscando no existe.",
+  "share.error.access-denied.title": "Private share",
+  "share.error.access-denied.description": "The current account does not have permission to access this share",
   "share.modal.password.title": "Se requiere contraseña",
   "share.modal.password.description": "Por favor ingrese la contraseña para poder acceder a este compartido.",
   "share.modal.password": "Contraseña",
