cb19e7d0700b64385a6e763e768ac2673d6b2198
Elias Schneider <login@eliasschneider.com>
2024-07-30 08:46:56 +0200

    New translations en-us.ts (Serbian (Cyrillic))

--- frontend/src/i18n/translations/sr-SP.ts
+++ frontend/src/i18n/translations/sr-SP.ts
@@ -152,6 +152,10 @@ export default {
   "account.reverseShares.modal.max-size.label": "Максимална величина дељења",
   "account.reverseShares.modal.send-email": "Пошаљите обавештење путем е-поште",
   "account.reverseShares.modal.send-email.description": "Пошаљите обавештење е-поштом када се креира дељење помоћу ове обрнуте везе за дељење.",
+  "account.reverseShares.modal.simplified": "Simple mode",
+  "account.reverseShares.modal.simplified.description": "Make it easy for the person uploading the file to share it with you. They will be able to customize only the name and description of the share.",
+  "account.reverseShares.modal.public-access": "Public access",
+  "account.reverseShares.modal.public-access.description": "Make the created shares with this reverse share public. If disabled, only you and the creator of the share can view it.",
   "account.reverseShares.modal.max-use.label": "Максималан број коришћења",
   "account.reverseShares.modal.max-use.description": "Максималан број пута који овај URL може да се користи за прављење дељења.",
   "account.reverseShare.never-expires": "Ово обрнуто дељење никада неће истећи.",
@@ -255,6 +259,7 @@ export default {
   "upload.modal.completed.never-expires": "Ово дељење никада неће истећи.",
   "upload.modal.completed.expires-on": "Ово дељење ће истећи {expiration}.",
   "upload.modal.completed.share-ready": "Дељење је спремно",
+  "upload.modal.completed.notified-reverse-share-creator": "We have notified the creator of the reverse share. You can also manually share this link with them through other means.",
   // END /upload
   // /share/[id]
   "share.title": "Дељење {shareId}",
@@ -264,6 +269,8 @@ export default {
   "share.error.removed.title": "Дељење је уклоњено",
   "share.error.not-found.title": "Дељење није пронађено",
   "share.error.not-found.description": "Удео који тражите не постоји.",
+  "share.error.access-denied.title": "Private share",
+  "share.error.access-denied.description": "The current account does not have permission to access this share",
   "share.modal.password.title": "Обавезна лозинка",
   "share.modal.password.description": "Да бисте приступили овом дељењу, унесите лозинку за дељење.",
   "share.modal.password": "Лозинка",
