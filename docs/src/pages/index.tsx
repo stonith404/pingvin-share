1daac10a6e8cd81fc420e8735ff13f2075534490
Elias Schneider <login@eliasschneider.com>
2024-08-24 17:06:40 +0200

    fix wrong redirection path

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -2,5 +2,5 @@ import React from 'react';
 import  { Redirect } from 'react-router-dom';
 
 export default function Home() {
-  return <Redirect to='/introduction' />;
+  return <Redirect to='/pingvin-share/introduction' />;
 }
