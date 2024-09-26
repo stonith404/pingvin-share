545158d546ba6816ea98007cb8a632ef30d98d22
WolverinDEV <git@did.science>
2024-09-26 14:58:31 +0900

    Revert "fix: disable email login if ldap is enabled"
    This reverts commit d9cfe697d66e9db7bfbc2252b3700580793ce9bb.

--- frontend/src/i18n/translations/en-US.ts
+++ frontend/src/i18n/translations/en-US.ts
@@ -50,7 +50,6 @@ export default {
   "signIn.oauth.microsoft": "Microsoft",
   "signIn.oauth.discord": "Discord",
   "signIn.oauth.oidc": "OpenID",
-  "signIn.error.invalid-username": "Invalid username",
 
   // END /auth/signin
 
