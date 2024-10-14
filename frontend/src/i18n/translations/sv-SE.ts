5dd93aa462bde6a3b2605674452ac368a95da843
Elias Schneider <login@eliasschneider.com>
2024-10-14 18:18:20 +0200

    New translations en-us.ts (Swedish)

--- frontend/src/i18n/translations/sv-SE.ts
+++ frontend/src/i18n/translations/sv-SE.ts
@@ -392,6 +392,8 @@ export default {
   "admin.config.oauth.oidc-enabled.description": "Om OpenID-inloggning är aktiverat",
   "admin.config.oauth.oidc-discovery-uri": "OpenID Connect Discovery URI",
   "admin.config.oauth.oidc-discovery-uri.description": "Discovery URI för OpenID Connect OAuth appen",
+  "admin.config.oauth.oidc-sign-out": "Sign out from OpenID Connect",
+  "admin.config.oauth.oidc-sign-out.description": "Whether the “Sign out” button will sign out from the OpenID Connect provider",
   "admin.config.oauth.oidc-username-claim": "OpenID Connect användarnamnsanspråk",
   "admin.config.oauth.oidc-username-claim.description": "Användarnamnsanspråk i OpenID Connect ID token. Lämna tomt om du inte vet vad denna konfiguration är.",
   "admin.config.oauth.oidc-role-path": "Path to roles in OpenID Connect token",
