d536276a9f1c3dca1ee6dd71d7932ebd18f615fd
Elias Schneider <login@eliasschneider.com>
2024-10-18 16:14:41 +0200

    New translations en-us.ts (Vietnamese)

--- frontend/src/i18n/translations/vi-VN.ts
+++ frontend/src/i18n/translations/vi-VN.ts
@@ -382,6 +382,8 @@ export default {
   "admin.config.oauth.microsoft-client-secret.description": "Client secret of the Microsoft OAuth app",
   "admin.config.oauth.discord-enabled": "Discord",
   "admin.config.oauth.discord-enabled.description": "Whether Discord login is enabled",
+  "admin.config.oauth.discord-limited-users": "Discord limited users",
+  "admin.config.oauth.discord-limited-users.description": "Limit signing in to specific users by their Discord ID. Leave it blank to disable.",
   "admin.config.oauth.discord-limited-guild": "Discord limited server ID",
   "admin.config.oauth.discord-limited-guild.description": "Limit signing in to users in a specific server. Leave it blank to disable.",
   "admin.config.oauth.discord-client-id": "Discord Client ID",
