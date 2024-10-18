1bf38aeec3bf78b66551113931847f27d0279b57
Elias Schneider <login@eliasschneider.com>
2024-10-18 16:15:02 +0200

    New translations en-us.ts (Chinese Traditional)

--- frontend/src/i18n/translations/zh-TW.ts
+++ frontend/src/i18n/translations/zh-TW.ts
@@ -382,6 +382,8 @@ export default {
   "admin.config.oauth.microsoft-client-secret.description": "Microsoft OAuth 的 Client secret",
   "admin.config.oauth.discord-enabled": "Discord",
   "admin.config.oauth.discord-enabled.description": "啟用 Discord 登入",
+  "admin.config.oauth.discord-limited-users": "Discord limited users",
+  "admin.config.oauth.discord-limited-users.description": "Limit signing in to specific users by their Discord ID. Leave it blank to disable.",
   "admin.config.oauth.discord-limited-guild": "Discord limited server ID",
   "admin.config.oauth.discord-limited-guild.description": "Limit signing in to users in a specific server. Leave it blank to disable.",
   "admin.config.oauth.discord-client-id": "Discord Client ID",
