fbe8792740bd768ff0539baf90c48c6f08af5661
Elias Schneider <login@eliasschneider.com>
2024-10-18 16:15:05 +0200

    New translations en-us.ts (Dutch, Belgium)

--- frontend/src/i18n/translations/nl-BE.ts
+++ frontend/src/i18n/translations/nl-BE.ts
@@ -382,6 +382,8 @@ export default {
   "admin.config.oauth.microsoft-client-secret.description": "Client-secret van de Microsoft OAuth app",
   "admin.config.oauth.discord-enabled": "Discord",
   "admin.config.oauth.discord-enabled.description": "Ofdat discord login is ingeschakeld",
+  "admin.config.oauth.discord-limited-users": "Discord limited users",
+  "admin.config.oauth.discord-limited-users.description": "Limit signing in to specific users by their Discord ID. Leave it blank to disable.",
   "admin.config.oauth.discord-limited-guild": "Discord beperkte server ID",
   "admin.config.oauth.discord-limited-guild.description": "Beperk het inloggen tot gebruikers in een specifieke server. Laat leeg om uit te schakelen.",
   "admin.config.oauth.discord-client-id": "Discord Client ID",
