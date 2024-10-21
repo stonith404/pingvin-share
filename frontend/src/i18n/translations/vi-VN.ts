27cab3fd13e297194717a0b2095442c9746dd69d
Elias Schneider <login@eliasschneider.com>
2024-10-21 09:14:01 +0200

    New translations en-us.ts (Vietnamese)

--- frontend/src/i18n/translations/vi-VN.ts
+++ frontend/src/i18n/translations/vi-VN.ts
@@ -190,7 +190,7 @@ export default {
   "admin.users.edit.update.change-password.button": "Đổi mật khẩu",
   "admin.users.edit.update.notify.password.success": "Thay đổi mật khẩu thành công",
   "admin.users.edit.delete.title": "Delete user: {username} ?",
-  "admin.users.edit.delete.description": "Do you really want to delete this user and all his shares?",
+  "admin.users.edit.delete.description": "Do you really want to delete this user and all their shares?",
   // showCreateUserModal.tsx
   "admin.users.modal.create.title": "Create user",
   "admin.users.modal.create.username": "Username",
