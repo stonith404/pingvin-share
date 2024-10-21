2cb4bdfa39785262a159d849b8a97b520fb296e0
Elias Schneider <login@eliasschneider.com>
2024-10-21 09:14:17 +0200

    New translations en-us.ts (Serbian (Cyrillic))

--- frontend/src/i18n/translations/sr-SP.ts
+++ frontend/src/i18n/translations/sr-SP.ts
@@ -190,7 +190,7 @@ export default {
   "admin.users.edit.update.change-password.button": "Снимите нову лозинку",
   "admin.users.edit.update.notify.password.success": "Лозинка је успешно промењена",
   "admin.users.edit.delete.title": "Delete user: {username} ?",
-  "admin.users.edit.delete.description": "Да ли заиста желите да избришете овог корисника и сва његова дељења?",
+  "admin.users.edit.delete.description": "Do you really want to delete this user and all their shares?",
   // showCreateUserModal.tsx
   "admin.users.modal.create.title": "Креирај корисника",
   "admin.users.modal.create.username": "Корисничко име",
