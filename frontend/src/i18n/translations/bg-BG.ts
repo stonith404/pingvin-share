cf18210adb9a4997f291542efddae7ceb3a1539e
Elias Schneider <login@eliasschneider.com>
2024-10-21 09:14:04 +0200

    New translations en-us.ts (Bulgarian)

--- frontend/src/i18n/translations/bg-BG.ts
+++ frontend/src/i18n/translations/bg-BG.ts
@@ -190,7 +190,7 @@ export default {
   "admin.users.edit.update.change-password.button": "Save new password",
   "admin.users.edit.update.notify.password.success": "Password changed successfully",
   "admin.users.edit.delete.title": "Delete user: {username} ?",
-  "admin.users.edit.delete.description": "Do you really want to delete this user and all his shares?",
+  "admin.users.edit.delete.description": "Do you really want to delete this user and all their shares?",
   // showCreateUserModal.tsx
   "admin.users.modal.create.title": "Create user",
   "admin.users.modal.create.username": "Username",
