5d6fa76d46b2215c18ba8afda1b0ac7d71147f3c
Elias Schneider <login@eliasschneider.com>
2024-10-21 09:14:09 +0200

    New translations en-us.ts (Finnish)

--- frontend/src/i18n/translations/fi-FI.ts
+++ frontend/src/i18n/translations/fi-FI.ts
@@ -190,7 +190,7 @@ export default {
   "admin.users.edit.update.change-password.button": "Tallenna uusi salasana",
   "admin.users.edit.update.notify.password.success": "Salasana vaihdettu",
   "admin.users.edit.delete.title": "Delete user: {username} ?",
-  "admin.users.edit.delete.description": "Haluatko varmasti poistaa tämän käyttäjän ja kaikki hänen jaot?",
+  "admin.users.edit.delete.description": "Do you really want to delete this user and all their shares?",
   // showCreateUserModal.tsx
   "admin.users.modal.create.title": "Luo käyttäjä",
   "admin.users.modal.create.username": "Käyttäjätunnus",
