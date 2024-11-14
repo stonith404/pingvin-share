211d36d578d8c2b294276eb387a5f931ea976371
Elias Schneider <login@eliasschneider.com>
2024-11-14 18:47:11 +0100

    New translations en-us.ts (Danish)

--- frontend/src/i18n/translations/da-DK.ts
+++ frontend/src/i18n/translations/da-DK.ts
@@ -212,8 +212,11 @@ export default {
   // END /admin/shares
   // /upload
   "upload.title": "Upload",
+  "upload.notify.confirm-leave": "Are you sure you want to leave this page? Your upload will be canceled.",
   "upload.notify.generic-error": "Der opstod en fejl under afslutningen af din deling.",
   "upload.notify.count-failed": "{count} files failed to upload. Trying again.",
+  "upload.reverse-share.error.invalid.title": "Invalid reverse share link",
+  "upload.reverse-share.error.invalid.description": "This reverse share has expired or is invalid.",
   // Dropzone.tsx
   "upload.dropzone.title": "Upload filer",
   "upload.dropzone.description": "Drag'n'drop files here to start your share. We only accept files up to {maxSize} in total.",
