7fe251fcf273c2f7a9fb1db4552695587443e1e2
Elias Schneider <login@eliasschneider.com>
2024-11-14 17:52:31 +0100

    New translations en-us.ts (Japanese)

--- frontend/src/i18n/translations/ja-JP.ts
+++ frontend/src/i18n/translations/ja-JP.ts
@@ -300,6 +300,8 @@ export default {
   "admin.config.general.app-name.description": "アプリの名前",
   "admin.config.general.app-url": "アプリ名",
   "admin.config.general.app-url.description": "Pingvin Shareで利用できるURL",
+  "admin.config.general.secure-cookies": "Secure cookies",
+  "admin.config.general.secure-cookies.description": "Whether to set the secure flag on cookies. If enabled, the site will not function when accessed over HTTP.",
   "admin.config.general.show-home-page": "ホームページを表示する",
   "admin.config.general.show-home-page.description": "ホームページを表示するかどうか選択",
   "admin.config.general.session-duration": "セッション期間",
