5705eafd75ad36b757f197fefc8fea0273dff0f0
Elias Schneider <login@eliasschneider.com>
2024-11-14 18:47:10 +0100

    New translations en-us.ts (Czech)

--- frontend/src/i18n/translations/cs-CZ.ts
+++ frontend/src/i18n/translations/cs-CZ.ts
@@ -212,8 +212,11 @@ export default {
   // END /admin/shares
   // /upload
   "upload.title": "Nahrát",
+  "upload.notify.confirm-leave": "Are you sure you want to leave this page? Your upload will be canceled.",
   "upload.notify.generic-error": "Při dokončování vašeho sdílení došlo k chybě.",
   "upload.notify.count-failed": "{count} souborů se nepodařilo nahrát. Zkouším to znovu.",
+  "upload.reverse-share.error.invalid.title": "Invalid reverse share link",
+  "upload.reverse-share.error.invalid.description": "This reverse share has expired or is invalid.",
   // Dropzone.tsx
   "upload.dropzone.title": "Nahrát soubory",
   "upload.dropzone.description": "Drag'n'drop files here to start your share. We only accept files up to {maxSize} in total.",
