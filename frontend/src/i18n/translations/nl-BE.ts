bd10484ab1f4612870091970efb470cbf4b29718
Elias Schneider <login@eliasschneider.com>
2024-11-17 16:45:58 +0100

    New translations en-us.ts (Dutch, Belgium)

--- frontend/src/i18n/translations/nl-BE.ts
+++ frontend/src/i18n/translations/nl-BE.ts
@@ -317,7 +317,7 @@ export default {
   "admin.config.email.share-recipients-subject": "Onderwerp van ontvangers' share",
   "admin.config.email.share-recipients-subject.description": "Het onderwerp van de e-mail die wordt verzonden naar de share-ontvangers.",
   "admin.config.email.share-recipients-message": "Bericht voor ontvangers van de share",
-  "admin.config.email.share-recipients-message.description": "Message which gets sent to the share recipients. Available variables:\n {creator} - The username of the creator of the share\n {shareUrl} - The URL of the share\n {desc} - The description of the share\n {expires} - The expiration date of the share\n These variables will be replaced with the actual value.",
+  "admin.config.email.share-recipients-message.description": "Message which gets sent to the share recipients. Available variables:\n {creator} - The username of the creator of the share\n {creatorEmail} - The email of the creator of the share\n {shareUrl} - The URL of the share\n {desc} - The description of the share\n {expires} - The expiration date of the share\n These variables will be replaced with the actual value.",
   "admin.config.email.reverse-share-subject": "Omgekeerde share email onderwerp",
   "admin.config.email.reverse-share-subject.description": "Subject of the sent email when someone created a share with your reverse share link.",
   "admin.config.email.reverse-share-message": "Omgekeerde share-bericht",
