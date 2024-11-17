01c8c9ffa95c9848520a7e5429fbced915f9fd40
Elias Schneider <login@eliasschneider.com>
2024-11-17 16:45:59 +0100

    New translations en-us.ts (Arabic, Egypt)

--- frontend/src/i18n/translations/ar-EG.ts
+++ frontend/src/i18n/translations/ar-EG.ts
@@ -317,7 +317,7 @@ export default {
   "admin.config.email.share-recipients-subject": "عنوان الرسالة لمستلمي المشاركة",
   "admin.config.email.share-recipients-subject.description": "عنوان البريد الذي سيُرسَل لمستقبِلي المشاركة.",
   "admin.config.email.share-recipients-message": "رسالتك لمستقبِلي المشاركة",
-  "admin.config.email.share-recipients-message.description": "Message which gets sent to the share recipients. Available variables:\n {creator} - The username of the creator of the share\n {shareUrl} - The URL of the share\n {desc} - The description of the share\n {expires} - The expiration date of the share\n These variables will be replaced with the actual value.",
+  "admin.config.email.share-recipients-message.description": "Message which gets sent to the share recipients. Available variables:\n {creator} - The username of the creator of the share\n {creatorEmail} - The email of the creator of the share\n {shareUrl} - The URL of the share\n {desc} - The description of the share\n {expires} - The expiration date of the share\n These variables will be replaced with the actual value.",
   "admin.config.email.reverse-share-subject": "عنوان المشاركة العكسية",
   "admin.config.email.reverse-share-subject.description": "Subject of the sent email when someone created a share with your reverse share link.",
   "admin.config.email.reverse-share-message": "رسالة المشاركة العكسية",
