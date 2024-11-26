94cb58c74e6d931427c9cd2c79418b28c0cba5f5
Elias Schneider <login@eliasschneider.com>
2024-11-26 21:17:52 +0100

    New translations en-us.ts (Greek)

--- frontend/src/i18n/translations/el-GR.ts
+++ frontend/src/i18n/translations/el-GR.ts
@@ -436,6 +436,9 @@ export default {
   "admin.config.ldap.field-name-member-of.description": "LDAP attribute name for the groups, an user is a member of. This is used when checking for the admin group.",
   "admin.config.ldap.field-name-email": "User email attribute name",
   "admin.config.ldap.field-name-email.description": "LDAP attribute name for the email of an user.",
+  "admin.config.notify.success": "Configuration updated successfully.",
+  "admin.config.notify.logo-success": "Logo updated successfully. It may take a few minutes to update on the website.",
+  "admin.config.notify.no-changes": "No changes to save.",
   // 404
   "404.description": "Ουπς. Αυτή η σελίδα δεν υπάρχει.",
   "404.button.home": "Πήγαινέ με πίσω",
@@ -477,6 +480,7 @@ export default {
   "common.button.go-back": "Επιστροφή",
   "common.button.go-home": "Μετάβαση στην αρχική",
   "common.notify.copied": "Ο σύνδεσμος σας αντιγράφηκε στο πρόχειρο",
+  "common.notify.copied-link": "Ο σύνδεσμος σας αντιγράφηκε στο πρόχειρο",
   "common.success": "Επιτυχία",
   "common.error": "Σφάλμα",
   "common.error.unknown": "Προέκυψε άγνωστο σφάλμα",
