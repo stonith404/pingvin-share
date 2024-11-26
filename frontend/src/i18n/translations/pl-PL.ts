7661f8b692607c84eb73cd8c396276d4a96a1fb1
Elias Schneider <login@eliasschneider.com>
2024-11-26 21:17:58 +0100

    New translations en-us.ts (Polish)

--- frontend/src/i18n/translations/pl-PL.ts
+++ frontend/src/i18n/translations/pl-PL.ts
@@ -436,6 +436,9 @@ export default {
   "admin.config.ldap.field-name-member-of.description": "LDAP attribute name for the groups, an user is a member of. This is used when checking for the admin group.",
   "admin.config.ldap.field-name-email": "User email attribute name",
   "admin.config.ldap.field-name-email.description": "LDAP attribute name for the email of an user.",
+  "admin.config.notify.success": "Configuration updated successfully.",
+  "admin.config.notify.logo-success": "Logo updated successfully. It may take a few minutes to update on the website.",
+  "admin.config.notify.no-changes": "No changes to save.",
   // 404
   "404.description": "Ups! Ta strona nie istnieje.",
   "404.button.home": "Wróć do strony domowej",
@@ -477,6 +480,7 @@ export default {
   "common.button.go-back": "Wróć",
   "common.button.go-home": "Wróć do ekranu głównego",
   "common.notify.copied": "Link został skopiowany do schowka",
+  "common.notify.copied-link": "Link został skopiowany do schowka",
   "common.success": "Zakończono pomyślnie",
   "common.error": "Błąd",
   "common.error.unknown": "Wystąpił nieznany błąd",
