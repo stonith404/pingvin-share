c464024d6d3ff42237190a2b6125c1399b759d56
Elias Schneider <login@eliasschneider.com>
2024-12-29 18:46:09 +0100

    New translations en-us.ts (Thai)

--- frontend/src/i18n/translations/th-TH.ts
+++ frontend/src/i18n/translations/th-TH.ts
@@ -124,6 +124,7 @@ export default {
   "account.shares.table.expiresAt": "Expires on",
   "account.shares.table.createdAt": "Created on",
   "account.shares.table.size": "ขนาด",
+  "account.shares.table.expiry-never": "Never",
   "account.shares.modal.share-informations": "ข้อมูลแชร์",
   "account.shares.modal.share-link": "แชร์ลิงค์",
   "account.shares.modal.delete.title": "Delete share: {share}",
