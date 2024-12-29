d69725321c626e27eac82ad33776b5bb7495e8b0
Elias Schneider <login@eliasschneider.com>
2024-12-29 18:46:00 +0100

    New translations en-us.ts (Turkish)

--- frontend/src/i18n/translations/tr-TR.ts
+++ frontend/src/i18n/translations/tr-TR.ts
@@ -124,6 +124,7 @@ export default {
   "account.shares.table.expiresAt": "Expires on",
   "account.shares.table.createdAt": "Created on",
   "account.shares.table.size": "Boyut",
+  "account.shares.table.expiry-never": "Never",
   "account.shares.modal.share-informations": "Bilgiler paylaşın",
   "account.shares.modal.share-link": "Bağlantıyı paylaş",
   "account.shares.modal.delete.title": "Delete share: {share}",
