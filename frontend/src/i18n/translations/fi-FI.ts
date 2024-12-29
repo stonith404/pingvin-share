90473a452829f59ead906320332a2cb767cdf71a
Elias Schneider <login@eliasschneider.com>
2024-12-29 18:45:52 +0100

    New translations en-us.ts (Finnish)

--- frontend/src/i18n/translations/fi-FI.ts
+++ frontend/src/i18n/translations/fi-FI.ts
@@ -124,6 +124,7 @@ export default {
   "account.shares.table.expiresAt": "Expires on",
   "account.shares.table.createdAt": "Created on",
   "account.shares.table.size": "Koko",
+  "account.shares.table.expiry-never": "Never",
   "account.shares.modal.share-informations": "Jaetun tiedot",
   "account.shares.modal.share-link": "Jaa linkki",
   "account.shares.modal.delete.title": "Delete share: {share}",
