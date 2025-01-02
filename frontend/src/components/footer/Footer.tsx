edb72d52a794c3cf6f0ff48c5d9d613152536c0d
Elias Schneider <login@eliasschneider.com>
2025-01-02 17:26:59 +0100

    improve mobile layout

--- frontend/src/components/footer/Footer.tsx
+++ frontend/src/components/footer/Footer.tsx
@@ -1,46 +1,62 @@
-import { Anchor, Grid, Footer as MFooter, Text } from "@mantine/core";
+import { Anchor, Footer as MFooter, SimpleGrid, Text } from "@mantine/core";
+import { useMediaQuery } from "@mantine/hooks";
 import useConfig from "../../hooks/config.hook";
 import useTranslate from "../../hooks/useTranslate.hook";
 
 const Footer = () => {
-	const t = useTranslate();
-	const config = useConfig();
-	const hasImprint = !!(config.get("legal.imprintUrl") || config.get("legal.imprintText"));
-	const hasPrivacy = !!(config.get("legal.privacyPolicyUrl") || config.get("legal.privacyPolicyText"));
-	const imprintUrl = (!config.get("legal.imprintText") && config.get("legal.imprintUrl")) || "/imprint";
-	const privacyUrl = (!config.get("legal.privacyPolicyText") && config.get("legal.privacyPolicyUrl")) || "/privacy";
-	return (
-		<MFooter height="auto" p={10} zIndex={100}>
-			<Grid>
-				<Grid.Col span={4} />
-				<Grid.Col span={4}>
-					<Text size="xs" color="dimmed" align="center">
-						Powered by{" "}
-						<Anchor size="xs" href="https://github.com/stonith404/pingvin-share" target="_blank">
-							Pingvin Share
-						</Anchor>
-					</Text>
-				</Grid.Col>
-				<Grid.Col span={4}>
-					{config.get("legal.enabled") && (
-						<Text size="xs" color="dimmed" align="right">
-							{hasImprint && (
-								<Anchor size="xs" href={imprintUrl}>
-									{t("imprint.title")}
-								</Anchor>
-							)}
-							{hasImprint && hasPrivacy && " • "}
-							{hasPrivacy && (
-								<Anchor size="xs" href={privacyUrl}>
-									{t("privacy.title")}
-								</Anchor>
-							)}
-						</Text>
-					)}
-				</Grid.Col>
-			</Grid>
-		</MFooter>
-	);
+  const t = useTranslate();
+  const config = useConfig();
+  const hasImprint = !!(
+    config.get("legal.imprintUrl") || config.get("legal.imprintText")
+  );
+  const hasPrivacy = !!(
+    config.get("legal.privacyPolicyUrl") ||
+    config.get("legal.privacyPolicyText")
+  );
+  const imprintUrl =
+    (!config.get("legal.imprintText") && config.get("legal.imprintUrl")) ||
+    "/imprint";
+  const privacyUrl =
+    (!config.get("legal.privacyPolicyText") &&
+      config.get("legal.privacyPolicyUrl")) ||
+    "/privacy";
+
+  const isMobile = useMediaQuery("(max-width: 700px)");
+
+  return (
+    <MFooter height="auto" py={6} px="xl" zIndex={100}>
+      <SimpleGrid cols={isMobile ? 2 : 3} m={0}>
+        {!isMobile && <div></div>}
+        <Text size="xs" color="dimmed" align={isMobile ? "left" : "center"}>
+          Powered by{" "}
+          <Anchor
+            size="xs"
+            href="https://github.com/stonith404/pingvin-share"
+            target="_blank"
+          >
+            Pingvin Share
+          </Anchor>
+        </Text>
+        <div>
+          {config.get("legal.enabled") && (
+            <Text size="xs" color="dimmed" align="right">
+              {hasImprint && (
+                <Anchor size="xs" href={imprintUrl}>
+                  {t("imprint.title")}
+                </Anchor>
+              )}
+              {hasImprint && hasPrivacy && " • "}
+              {hasPrivacy && (
+                <Anchor size="xs" href={privacyUrl}>
+                  {t("privacy.title")}
+                </Anchor>
+              )}
+            </Text>
+          )}
+        </div>
+      </SimpleGrid>
+    </MFooter>
+  );
 };
 
 export default Footer;
