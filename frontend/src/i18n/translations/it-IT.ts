754bb9245962426429f8dc9c81caa4c7496741b9
Elias Schneider <login@eliasschneider.com>
2025-01-04 13:52:45 +0100

    New translations en-us.ts (Italian)

--- frontend/src/i18n/translations/it-IT.ts
+++ frontend/src/i18n/translations/it-IT.ts
@@ -296,10 +296,10 @@ export default {
   "share.edit.notify.save-success": "Condivisione aggiornata correttamente",
   // END /share/[id]/edit
   // /imprint
-  "imprint.title": "Imprint",
+  "imprint.title": "Note legali",
   // END /imprint
   // /privacy
-  "privacy.title": "Privacy Policy",
+  "privacy.title": "Informativa sulla privacy",
   // END /privacy
   // /admin/config
   "admin.config.title": "Configurazione",
@@ -344,7 +344,7 @@ export default {
   "admin.config.share.allow-unauthenticated-shares": "Consenti condivisioni non autenticate",
   "admin.config.share.allow-unauthenticated-shares.description": "Indica se gli utenti non autenticati possono creare condivisioni",
   "admin.config.share.max-expiration": "Scadenza massima",
-  "admin.config.share.max-expiration.description": "Maximum share expiration. Set to 0 to allow unlimited expiration.",
+  "admin.config.share.max-expiration.description": "Scadenza massima della condivisione. Impostare a 0 per consentire la scadenza illimitata.",
   "admin.config.share.share-id-length": "Lunghezza ID condivisione predefinita",
   "admin.config.share.share-id-length.description": "Lunghezza predefinita dell'ID generato per una condivisione. Questo valore viene utilizzato anche per generare collegamenti per condivisioni inverse. Un valore inferiore a 8 non è considerato sicuro.",
   "admin.config.share.max-size": "Dim. massima",
@@ -463,17 +463,17 @@ export default {
   "admin.config.s3.key.description": "La chiave che consente di accedere al bucket S3.",
   "admin.config.s3.secret": "Parola chiave",
   "admin.config.s3.secret.description": "La parola chiave che consente di accedere al bucket S3.",
-  "admin.config.category.legal": "Legal",
-  "admin.config.legal.enabled": "Enable legal notices",
-  "admin.config.legal.enabled.description": "Whether to show a link to imprint and privacy policy in the footer.",
-  "admin.config.legal.imprint-text": "Imprint text",
-  "admin.config.legal.imprint-text.description": "The text which should be shown in the imprint. Supports Markdown. Leave blank to link to an external imprint page.",
-  "admin.config.legal.imprint-url": "Imprint URL",
-  "admin.config.legal.imprint-url.description": "If you already have an imprint page you can link it here instead of using the text field.",
-  "admin.config.legal.privacy-policy-text": "Privacy policy text",
-  "admin.config.legal.privacy-policy-text.description": "The text which should be shown in the privacy policy. Supports Markdown. Leave blank to link to an external privacy policy page.",
-  "admin.config.legal.privacy-policy-url": "Privacy policy URL",
-  "admin.config.legal.privacy-policy-url.description": "If you already have a privacy policy page you can link it here instead of using the text field.",
+  "admin.config.category.legal": "Legale",
+  "admin.config.legal.enabled": "Abilita le notifiche legali",
+  "admin.config.legal.enabled.description": "Indica se mostrare o meno un link alle informative legali e sulla privacy a piè di pagina.",
+  "admin.config.legal.imprint-text": "Testo dell'informativa",
+  "admin.config.legal.imprint-text.description": "Il testo che deve essere mostrato nell'informativa. Supporta il linguaggio Markdown. Lascia vuoto per collegare una pagina informativa esterna.",
+  "admin.config.legal.imprint-url": "URL dell'informativa",
+  "admin.config.legal.imprint-url.description": "Nel caso avessi già una pagina di informativa puoi collegarla qui invece di usare il campo di testo.",
+  "admin.config.legal.privacy-policy-text": "Testo dell'informativa sulla privacy",
+  "admin.config.legal.privacy-policy-text.description": "Il testo che dovrebbe essere mostrato nell'informativa sulla privacy. Supporta il linguaggio Markdown. Lascia vuoto per collegare una pagina esterna inerente la politica sulla privacy.",
+  "admin.config.legal.privacy-policy-url": "URL dell'informativa sulla privacy",
+  "admin.config.legal.privacy-policy-url.description": "Nel caso avessi già una pagina di informativa sulla privacy, puoi collegarla qui invece di usare il campo di testo.",
   // 404
   "404.description": "Ops, questa pagina non esiste.",
   "404.button.home": "Riportami a casa",
