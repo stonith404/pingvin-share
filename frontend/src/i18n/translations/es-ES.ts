7722bb094bc5e7eaaaf1a52c319a6f0b9577fd22
Elias Schneider <login@eliasschneider.com>
2025-02-28 11:02:50 +0100

    New translations en-us.ts (Spanish)

--- frontend/src/i18n/translations/es-ES.ts
+++ frontend/src/i18n/translations/es-ES.ts
@@ -302,6 +302,8 @@ export default {
   "privacy.title": "Política de privacidad",
   // END /privacy
   // /admin/config
+  "admin.config.config-file-warning.title": "Configuration file present",
+  "admin.config.config-file-warning.description": "As you have a configured Pingvin Share with a configuration file, you can't change the configuration through the UI.",
   "admin.config.title": "Configuración",
   "admin.config.category.general": "General",
   "admin.config.category.share": "Compartido",
